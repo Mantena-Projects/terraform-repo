573f6565e5bb9202cfedaa711cfc6e9cc538d0f6
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-08-24 10:42:30 +0100

    Add files via upload

--- aws-architecture/route-table.tf
+++ aws-architecture/route-table.tf
@@ -14,6 +14,10 @@ resource "aws_route_table" "public_subnet_route_table" {
 # Create a route tables for the private subnets
 resource "aws_route_table" "private_subnet_route_table" {
   vpc_id = aws_vpc.terraform_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_nat_gateway.id
+  }
   tags = {
     Name = "private-subnet-route-table"
   }
@@ -25,27 +29,23 @@ resource "aws_route_table" "private_subnet_route_table" {
 resource "aws_route_table_association" "public_subnet_1_association" {
   subnet_id      = aws_subnet.public_subnet-1.id
   route_table_id = aws_route_table.public_subnet_route_table.id
-  depends_on     = [aws_internet_gateway.terraform_igw]
 }
 
 # Associate public subnet-2 with public route tables
 resource "aws_route_table_association" "public_subnet_2_association" {
   subnet_id      = aws_subnet.public_subnet-2.id
   route_table_id = aws_route_table.public_subnet_route_table.id
-  depends_on     = [aws_internet_gateway.terraform_igw]
 }
 
 # Associate private subnet-1 with private route tables
 resource "aws_route_table_association" "private_subnet-1_association" {
   subnet_id      = aws_subnet.private_subnet-1.id
   route_table_id = aws_route_table.private_subnet_route_table.id
-  depends_on     = [aws_internet_gateway.terraform_igw]
 }
 
 # Associate private subnet-1 with private route tables
 resource "aws_route_table_association" "private_subnet-2_association" {
   subnet_id      = aws_subnet.private_subnet-2.id
   route_table_id = aws_route_table.private_subnet_route_table.id
-  depends_on     = [aws_internet_gateway.terraform_igw]
 }
 
